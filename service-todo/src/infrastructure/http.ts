20d01180b9bc61c3f4504a2517da2a91d6961023
Alberto De Agostini <alberto.deagostini@infinitaslearning.com>
2024-12-20 16:19:36 +0100

    better error handling

--- service-todo/src/infrastructure/http.ts
+++ service-todo/src/infrastructure/http.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { TodoNotFoundError } from "../domain/errors";
 import * as Sentry from "@sentry/node"
 
@@ -31,45 +31,33 @@ const initHttpAdapter = ({ todoApplication, config, logger }: { todoApplication:
         res.json(todo);
     });
 
-    app.put("/todos/:id/toggle", async (req, res) => {
+    app.put("/todos/:id/toggle", async (req, res, next) => {
         logger.info("PUT /todos/:id/toggle", req.params.id);
         try {
             const todo = await todoApplication.toggleTodoCompleted(req.params.id);
             res.json(todo);
         } catch (error) {
-            if (error instanceof TodoNotFoundError) {
-                res.status(404).send(error.message);
-            } else {
-                res.status(500).send("Internal server error");
-            }
+            next(error)
         }
     });
 
-    app.put("/todos/:id", async (req, res) => {
+    app.put("/todos/:id", async (req, res, next) => {
         logger.info("PUT /todos/:id", req.params.id, req.body.title);
         try {
             const todo = await todoApplication.updateTodoTitle(req.params.id, req.body.title);
             res.json(todo);
         } catch (error) {
-            if (error instanceof TodoNotFoundError) {
-                res.status(404).send(error.message);
-            } else {
-                res.status(500).send("Internal server error");
-            }
+            next(error)
         }
     });
 
-    app.delete("/todos/:id", async (req, res) => {
+    app.delete("/todos/:id", async (req, res, next) => {
         logger.info("DELETE /todos/:id", req.params.id);
         try {
             const todo = await todoApplication.deleteTodo(req.params.id);
             res.json(todo);
         } catch (error) {
-            if (error instanceof TodoNotFoundError) {
-                res.status(404).send(error.message);
-            } else {
-                res.status(500).send("Internal server error");
-            }
+            next(error);
         }
     });
 
@@ -78,17 +66,24 @@ const initHttpAdapter = ({ todoApplication, config, logger }: { todoApplication:
         throw new Error("My first Sentry error!");
     });
 
+    app.use(function notFoundHandler(req: Request, res: Response) {
+        res.status(404).send("Not found");
+    })
+
     // The error handler must be registered before any other error middleware and after all controllers
     Sentry.setupExpressErrorHandler(app);
 
-    // TODO: setup a 404 handler
-    // TODO: setup a error middleware
-    app.use(function onError(err, req, res, next) {
-        // The error id is attached to `res.sentry` to be returned
-        // and optionally displayed to the user for support.
-        res.statusCode = 500;
-        res.end(res.sentry + "\n");
-    });
+    // Error handler middleware, beware that this is not catching thrown (and uncaught) errors, we should do that too but it's not the purpose of this example
+    app.use(function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+        if (err instanceof TodoNotFoundError) {
+            res.status(404).send(err.message);
+        } else {
+            console.error(err.stack);
+            // sentry middleware already logs the error on Sentry
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    })
+    
 
     return {
         run: () => {
